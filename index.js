f8ee12a71c3245860ec51dd189603ef562c7548e
MateWojno <mateusz.k.wojno@gmail.com>
2023-06-16 10:28:28 +0200

    readme added

--- index.js
+++ index.js
@@ -1,57 +1,65 @@
-// function getComputerChoice() {
-//     // randomly returns "Rock" "Paper" or "Scissors";
-//     let choiceNum = Math.floor(Math.random() * 3) + 1; 
+function getComputerChoice() {
+    // randomly returns "Rock" "Paper" or "Scissors";
+    let choiceNum = Math.floor(Math.random() * 3) + 1; 
 
-//     if (choiceNum == 1) {
-//         return "Rock";
-//     } else if (choiceNum == 2) {
-//         return "Paper"
-//     } else {
-//         return "Scissors"
-//     }
-// }
+    if (choiceNum == 1) {
+        return "rock";
+    } else if (choiceNum == 2) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
     
 function getPlayerChoice(){
     let selection = prompt("Pick Rock, Paper or Scissors");
-    // if(selection != null) 
-    // {selection = selection.toLowerCase;}
-
-    // if (selection !== "rock" || 
-    //     selection !== "paper"|| 
-    //     selection !==  "scissors") {
-    //         console.log("Idiot");
-    //         return "Error";
-    //     }
-    return selection;
+    let playerSelection = selection.toLowerCase();
+    
+    return playerSelection;
     // it should be case-insensitive, 
     // you can input RoCk for example 
- 
 }
+
+
 getPlayerChoice();
-const playerSelection = getPlayerChoice();
-console.log(playerSelection);
+const playerChoice = getPlayerChoice();
+console.log(playerChoice);
 
 
-// // TEST ONLY - setter
-// getComputerChoice();
-// const computerSelection = getComputerChoice();
-// console.log(computerSelection);
-// // ==================
+// TEST ONLY - setter
+getComputerChoice();
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
+// ==================
  
 
 
-// function playRound(playerSelection, computerSelection) {
-//     // return a string that declares the winner like:
-//     // "You lose! Paper beats Rock"
-// }
+function playRound(playerChoice, computerChoice) {
+    if (computerChoice == playerChoice) {
+        return "Draw!";
+    } else {
+        return "TODO";
+    }
+    
+    // TODO
+    // switch (playerChoice === getComputerChoice) {
+    //     case value:
+            
+    //         break;
+    
+    //     default:
+    //         break;
+    }// return a string that declares the winner like:
+    // "You lose! Paper beats Rock"
+}
 
-// const playerSelection = "rock";
-// console.log(playRound(playerSelection, computerSelection));
+const playerSelection = "rock";
+console.log(playRound(playerSelection, computerSelection));
 
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         playRound(playerSelection, computerSelection);
-//         console.log(playRound(playerSelection, computerSelection));
-//     }
-// }
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
 
